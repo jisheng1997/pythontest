d5d70cf6cd7073058fa96570f6ba8c825f3eaecf
jisheng1997 <jsthw1225>
2020-08-13 00:38:45 +0800

    ver1.6.1

--- Oct_12.py
+++ Oct_12.py
@@ -11,7 +11,7 @@ import json
 from lxml import etree
 
 path1 = "https://www.taobao.com"
-path2 = "https://httpbin.org/get"
+path2 = "https://httpbin.org/"
 path3 = "https://github.com/timeline.json"
 path4 = "http://ibeifeng.com"
 dict1 = {"username":"jisheng"
@@ -19,14 +19,18 @@ dict1 = {"username":"jisheng"
          ,"age":24,"sex":"man"}
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
 # res1 = requests.get(path1,headers = headers)
-# res2 = requests.get(path2,headers = headers,params=dict1)
+# res2 = requests.get(path2+'get',headers = headers,params=dict1)
 # res3 = requests.get(path3,headers = headers,stream = True)
-res4 = requests.get(path4,headers = headers)
-
+# res4 = requests.get(path4,headers = headers)
+# res5 = requests.get(path2+'cookies',cookies = {'name':'jisheng'})
+  
 
 # print(res3.json())
 # print(res4.raw.read(100))
 # print(res4.cookies)
 # print(res4.cookies['ECS[visit_times]'])
+# print(res5.text)
+
+
 
 
