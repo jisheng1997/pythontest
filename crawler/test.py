46461930715382c41fda93238404068a286b6688
jisheng <2551764920@qq.com>
2020-08-12 16:27:30 +0800

    ver1.6

--- crawler/test.py
+++ crawler/test.py
@@ -8,8 +8,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="https://www.pixiv.net/"
+url="https://www.taobao.com/"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'}
 html = requests.get(url,headers=headers)
 val = BeautifulSoup(html.text, 'html.parser')
-print(val.title)
+print(val.find())
